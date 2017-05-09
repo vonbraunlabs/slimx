09f6279d001c33f913c127ffea3e551e76118155
Diogo Oliveira de Melo <dmelo87@gmail.com>
2017-05-08 22:22:59 -0300

    Add skeleton for tests and travisci yml file

---
+++ tests/SlimX/SelfTests/SanityTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SlimX\SelfTests;
+
+use PHPUnit\Framework\TestCase;
+
+class SanityTest extends TestCase
+{
+    public function testSanity()
+    {
+        $this->assertTrue(true, "Assert the tests are working");
+    }
+}
